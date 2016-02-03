c881970f6a6821d200ac7b2190097682f89e1d63
sauvan_k <kevin.sauvanaud@epitech.eu>
2016-02-03 21:00:47 +0100

    register

---
+++ app/register/M_register.php
@@ -0,0 +1,28 @@
+<?php
+include('../database.php');
+
+error_reporting(E_ALL & E_NOTICE);
+ini_set('display_errors', 'on');
+class registerSQL
+{
+	function registerAllReadyExiste($login,$mail)
+	{
+		$connexion = \App\Model\Database::get()->prepare("SELECT * from tp_users
+			WHERE login = '".$login."' OR mail = '".$mail."'");
+		$connexion->execute();	
+		$data = $connexion->fetchAll();
+		return $data;	
+	}
+
+	function register($login,$pass,$mail,$prenom,$nom,$city)
+	{	
+		$connexion = \App\Model\Database::get()->prepare("INSERT INTO tp_users
+			(mail,login,password,first_name,last_name,city,register)
+			VALUE ( '".$mail."',  '".$login."', '".$pass."', '".$prenom."', '".$nom."','".$city."', NOW()  )");
+		$connexion->execute();
+		echo "true";
+	}
+}
+
+
+?>
